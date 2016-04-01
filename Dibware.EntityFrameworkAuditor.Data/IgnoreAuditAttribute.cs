5fd8f42e77d7c0480dbcfdaad604e69d3f52f3c6
dibley1973 <dibley1973@yahoo.co.uk>
2016-04-01 21:32:55 +0100

    20160401_2132_DWLT Still very much a work in progress

---
+++ Dibware.EntityFrameworkAuditor.Data/IgnoreAuditAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Dibware.EntityFrameworkAuditor.Data
+{
+    /// <summary>
+    /// This attribute can be placed on a class or property to prevent the 
+    /// auto auditing function including the class or property.
+    /// </summary>
+    /// <seealso cref="System.Attribute" />
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Property)]
+    public class IgnoreAuditAttribute : Attribute
+    {
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Dibware.EntityFrameworkAuditor
 An auto auditing DbContext for Entity Framework
+
+## Purpose
+The purpose of the primary assembly is to provide aDbContext that can be inherited from and will provide an audit log of changes to entities automatically 
